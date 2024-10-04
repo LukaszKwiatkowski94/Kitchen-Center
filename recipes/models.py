887458b4966d3c214a2f39d31dbed74dc675d0b0
Lukasz Kwiatkowski <kwiatkowskilukasz1994@gmail.com>
2024-10-04 21:23:45 +0200

    Added simply model for recipes

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Name")
+    active = models.BooleanField(default=True,verbose_name="Active")
+
+    def __str__(self):
+		return self.name
+
+class Recipes(models.Model):
+    name = models.CharField(max_length=250, verbose_name="Name")
+    description = models.TextField(blank=True, verbose_name="Description")
+    dateCreated = models.DateTimeField(default=datetime.datetime.now, verbose_name="Date of creation")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    active = models.BooleanField(default=True, verbose_name="Active")
+    photo = models.FileField(upload_to='newsPhoto/', verbose_name="Photo")
+    author=models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+		return self.name
